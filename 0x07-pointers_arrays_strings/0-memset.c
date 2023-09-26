e9a8597e3184684707bc146eefa90e6320aa5da5
kamaraabubakarrm <kamaraabubakarrm@gmail.com>
2023-09-25 21:54:11 -0700

    0-memset modified

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,17 +5,14 @@
  * @s: input pointer to string
  * @b: constant char
  * @n: number of bytes
- * Return: pointer to s string
+ * Return: pointer to s String
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		s[i] = b;
-		i++;
-	}
 	return (s);
 }
