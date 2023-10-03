02b1a72abb9a270f0e652143fd0b5594821b87e6
kamaraabubakarrm <kamaraabubakarrm@gmail.com>
2023-10-02 21:35:10 -0700

    Tasks completed

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,29 @@
+:set paste
+
+#include <string.h>
+
+/**
+ * _strpbrk - a function ...
+ * @s: the chaine
+ * @accept: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) || (accept == NULL))
+		return (NULL);
+	while (*s)
+	{
+		if (strchr(accept, *s))
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return (NULL);
+}
